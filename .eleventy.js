b4ef25a25f1191e009832b181a6d7b114607656f
rocc-o <89341472+rocc-o@users.noreply.github.com>
2021-08-28 18:32:53 +0200

    A few random tweaks #2

--- .eleventy.js
+++ .eleventy.js
@@ -1,92 +1,111 @@
 
 module.exports = function (eleventyConfig) {
 
+
+  const now = Date.now();
+  const livePosts = (post) => post.date <= now;
+
+  eleventyConfig.addFilter("tagsList", (arr = []) => {
+    const tagsSet = new Set();
+    arr.forEach((item) => item.data.tags?.forEach((tag) => tagsSet.add(tag)));
+    return [...tagsSet].sort((b, a) => b.localeCompare(a));
+  });
+
+  eleventyConfig.addFilter("inspect", require("util").inspect);
+
+
+
   // SIGHTS - custom collections
-  const nowsights = new Date();
-  const sightslivePosts = (post) => post.date <= nowsights;
+
   eleventyConfig.addCollection("sights", (collection) => {
-    return [
-      ...collection.getFilteredByGlob("./sights/*.njk").filter(sightslivePosts),
-    ].reverse();
+    const posts = collection.getFilteredByGlob("./sights/*.njk");
+    return [...posts].filter(livePosts).reverse();
   });
 
   // SOUNDS - custom collections
-  const nowsounds = new Date();
-  const soundslivePosts = (post) => post.date <= nowsounds;
+
   eleventyConfig.addCollection("sounds", (collection) => {
-    return [
-      ...collection.getFilteredByGlob("./sounds/*.njk").filter(soundslivePosts),
-    ].reverse();
+    const posts = collection.getFilteredByGlob("./sounds/*.njk");
+    return [...posts].filter(livePosts).reverse();
   });
 
   // generate a list of all tags collections
   // with alphabetical sorting - had to invert to "b, a" because it was sorting upside down
-  eleventyConfig.addCollection('tagsList', (collectionApi) => {
-    const tagsSet = new Set()
-    collectionApi.getAll().forEach((item) => {
-      if (!item.data.tags) return
-      item.data.tags.forEach((tag) => tagsSet.add(tag))
-    })
-    return  [...tagsSet].sort((b, a) => b.localeCompare(a))
+  eleventyConfig.addCollection("sightsTagsList", (collection) => {
+    const tagsListFilter = eleventyConfig.getFilter("tagsList");
+    const items = collection
+      .getFilteredByGlob("./sights/*.njk")
+      .filter(livePosts);
+    return tagsListFilter(items);
   });
 
 
-// https://www.11ty.dev/docs/copy/#manual-passthrough-file-copy-(faster)
-eleventyConfig.addPassthroughCopy('assets');
-eleventyConfig.addPassthroughCopy('sitemap.xml');
-eleventyConfig.addPassthroughCopy('robots.txt');
+  eleventyConfig.addCollection("soundsTagsList", (collection) => {
+    const tagsListFilter = eleventyConfig.getFilter("tagsList");
+    const items = collection
+      .getFilteredByGlob("./sounds/*.njk")
+      .filter(livePosts);
+    return tagsListFilter(items);
+  });
 
 
-// format dates on SIGHTS and SOUNDS Articles
-const dateformat = require('./lib/filters/dateformat');
-eleventyConfig.addFilter('datefriendly', dateformat.friendly);
-eleventyConfig.addFilter('dateymd', dateformat.ymd);
+  eleventyConfig.addFilter("head", (arr = [], idx = 0) => {
+    if (idx < 0) {
+      return arr.slice(idx);
+    }
+    return arr.slice(0, idx);
+  });
 
 
-// "return" in order to keep the previously entered configuration values
-// taken from: https://github.com/11ty/eleventy-base-blog/blob/master/.eleventy.js
-return {
-  // Control which files Eleventy will process
-  // e.g.: *.md, *.njk, *.html, *.liquid
-  templateFormats: [
-    "md",
-    "njk",
-    "html",
-    "liquid"
-  ],
+  // https://www.11ty.dev/docs/copy/#manual-passthrough-file-copy-(faster)
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("sitemap.xml");
+  eleventyConfig.addPassthroughCopy("robots.txt");
 
-// added
-  passthroughFileCopy: true,
+  // format dates on SIGHTS and SOUNDS Articles
+  const dateformat = require("./lib/filters/dateformat");
+  eleventyConfig.addFilter("datefriendly", dateformat.friendly);
+  eleventyConfig.addFilter("dateymd", dateformat.ymd);
 
-  // -----------------------------------------------------------------
-  // If your site deploys to a subdirectory, change `pathPrefix`.
-  // Don’t worry about leading and trailing slashes, we normalize these.
+  // "return" in order to keep the previously entered configuration values
+  // taken from: https://github.com/11ty/eleventy-base-blog/blob/master/.eleventy.js
+  return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: ["md", "njk", "html", "liquid"],
 
-  // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-  // This is only used for link URLs (it does not affect your file structure)
-  // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
+    // added
+    passthroughFileCopy: true,
 
-  // You can also pass this in on the command line using `--pathprefix`
+    // -----------------------------------------------------------------
+    // If your site deploys to a subdirectory, change `pathPrefix`.
+    // Don’t worry about leading and trailing slashes, we normalize these.
 
-  // Optional (default is shown)
-  pathPrefix: "/",
-  // -----------------------------------------------------------------
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for link URLs (it does not affect your file structure)
+    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
 
-  // Pre-process *.md files with: (default: `liquid`)
-  markdownTemplateEngine: "njk",
+    // You can also pass this in on the command line using `--pathprefix`
 
-  // Pre-process *.html files with: (default: `liquid`)
-  htmlTemplateEngine: "njk",
+    // Optional (default is shown)
+    pathPrefix: "/",
+    // -----------------------------------------------------------------
 
-  // Opt-out of pre-processing global data JSON files: (default: `liquid`)
-  dataTemplateEngine: false,
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: "njk",
 
-  // These are all optional (defaults are shown):
-  dir: {
-    input: ".",
-    includes: "_includes",
-    data: "_data",
-    output: "_site"
-  }
-};
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: "njk",
+
+    // Opt-out of pre-processing global data JSON files: (default: `liquid`)
+    dataTemplateEngine: false,
+
+    // These are all optional (defaults are shown):
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
 };
